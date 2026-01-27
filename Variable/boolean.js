a9d422f1c3f57a529a8b121e3920b1cef3b0c724
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-01-27 19:51:36 +0600

    feat: boolean

---
+++ Variable/boolean.js
@@ -0,0 +1,14 @@
+const crush_mgs = true;
+console.log(crush_mgs);
+
+const celebrating = true;
+console.log(celebrating);
+
+const isHappy = true;
+console.log(isHappy);
+
+const hasMoney = false;
+console.log(hasMoney);
+
+const isHungry = true;
+console.log(isHungry);
