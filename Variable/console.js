60fe6c99ee3fc75b93f294d836a8c475db64648d
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-01-27 19:31:17 +0600

    feat: console

---
+++ Variable/console.js
@@ -0,0 +1,18 @@
+const age = 20;
+console.log(age);
+
+const booksToRead = 1;
+console.log(booksToRead);
+
+let wakeUpTime = 8;
+wakeUpTime = 9;
+console.log(wakeUpTime);
+
+const wastedTime = 1;
+console.log(wastedTime);
+
+const borrow = 5000;
+console.log(borrow);
+
+const pillow = 2;
+console.log(pillow);
