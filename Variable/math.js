a09f323ad9879dc03fda2f2b2039a74d48e7ab82
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-02-09 21:52:47 +0600

    basic math problem solving

---
+++ Variable/math.js
@@ -0,0 +1,102 @@
+const eggPrice = 40;
+const onionPrice = 20;
+// const total = eggPrice + onionPrice;
+// console.log(total);
+
+// const result = eggPrice - onionPrice;
+// console.log(result);
+
+// console.log(eggPrice-onionPrice);
+
+// console.log(onionPrice * eggPrice);
+
+// console.log(10 / 3);
+// console.log(10 % 3);
+
+// const first = 'Tanjila';
+// const second = 'khatun';
+// const fullName = first + second;
+// console.log(fullName);
+
+// const suger = 20;
+// const money = '30';
+// console.log(suger + money);
+
+// const price = 35;
+// const inboxEasenPrice = price + 10;
+// const newPrice = price + 50;
+// const discountedPrice = newPrice - 10;
+// console.log(discountedPrice);
+
+/**
+ *        + addition
+ *        - subtraction
+ *        * multipication
+ *        ** Exponentiation
+ *        / division
+ *        % modules(remainder)
+ */
+
+// const income = 100;
+// const khoroc = 40;
+// const bakiTK = income - khoroc;
+// console.log(bakiTK);
+
+// const pencil = 10;
+// const use = 2;
+// const totalUse = pencil / use;
+// console.log(totalUse);
+
+// const myTaka = 60;
+// const everyTime = 15;
+// const manyTimes = myTaka / everyTime;
+// console.log(manyTimes);
+
+// const remain = myTaka % everyTime;
+// console.log(remain);
+
+// const chalKg = 153;
+// const dalKg = 261;
+// const total = chalKg + dalKg;
+// console.log(total);
+
+// const myTk = 500;
+// const myCost = 75;
+// const manyTimes = myTk / myCost;
+// console.log(manyTimes);
+
+// const remain = myTk % myCost;
+// console.log(remain);
+
+// const firstWord = 'Hello';
+// const lastWord = 'World';
+// const fullWord = firstWord + lastWord;
+// console.log(fullWord);
+
+// const firstNumber = 8;
+// const lastNumber = 3;
+// const division = firstNumber / lastNumber;
+// console.log('Division:', division);
+
+// const remainder = firstNumber % lastNumber;
+// console.log('Remainder:', remainder);
+
+// const firstNum = 50;
+// const lastNum = 9;
+// const remain = firstNum % lastNum;
+// console.log("Remainder:",remain);
+   
+
+// const word1 = 'Bangla';
+// const word2 = 'desh';
+// const word = word1 + word2;
+// console.log('Full word:', word);
+
+const total = 480;
+const everyDay = 4;
+const  howManyDays = total / everyDay;
+console.log('Total day:', howManyDays);
+
+const monthDays = 30;
+const totalMonth = howManyDays / monthDays;
+console.log(totalMonth);
