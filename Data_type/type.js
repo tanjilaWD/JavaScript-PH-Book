07eb29d2dfe0a596240a8261966e228f81d19fbf
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-02-09 22:19:31 +0600

    data type

---
+++ Data_type/type.js
@@ -0,0 +1,34 @@
+// const passed = false;
+// console.log(typeof passed);
+
+// const country = "Bangladesh";
+// const passed = true;
+// const price = 555;
+// console.log(typeof country);
+// console.log(typeof passed);
+// console.log(typeof price);
+
+/**
+ * Number: 1, 2, 3, 4, 5
+ * String: "English", "Hello World"
+ * Boolean: true, false
+ */
+
+// const price = 555;
+// console.log(typeof price);
+
+// const learning = true;
+// console.log(learning);
+// console.log(typeof learning);
+
+// const megaCity = "Dhaka";
+// console.log(typeof megaCity);
+
+// const temperature = 102.5;
+// console.log(typeof temperature);
+
+// const isHungry = true;
+// console.log(typeof isHungry);
+
+const isRich = true;
+console.log(typeof isRich);
