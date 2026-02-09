5e1daa6964c60eacd64ca0cf0d9dabb497cdb7c2
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-02-09 22:41:18 +0600

    let const

---
+++ Data_type/let_const.js
@@ -0,0 +1,41 @@
+// let price = 35;
+// price = 45;
+// console.log(price);
+
+// let balance = 500;
+// balance = 400;
+// console.log(balance);
+
+// const name = 'Kala Mia';
+// name = 'Lal Mia';
+// console.log(name);
+
+// let age = 21;
+// console.log('My Age:', age);
+// age = 22;
+// console.log('My Age ', age);
+
+// let pocketMoney = 500;
+// pocketMoney = 150;
+// console.log(pocketMoney);
+
+// let studyTime = 4;
+// studyTime = 5;
+// console.log(studyTime);
+
+// let friendsCount = 3;
+// friendsCount = 1;
+// console.log(friendsCount);
+
+// let phonePrice = 25000;
+// phonePrice = 12000;
+// console.log(phonePrice);
+
+// let chairsCount = 40;
+// chairsCount = chairsCount - 2;
+// chairsCount = chairsCount - 6;
+// console.log('Total chairs ->', chairsCount);
+
+let playTime = 3;
+playTime = 0;
+console.log('My play time:', playTime);
