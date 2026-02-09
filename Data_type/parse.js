79ce6f06c3e454f2a119aefd0c18663120c6055b
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-02-09 23:05:07 +0600

    parse

---
+++ Data_type/parse.js
@@ -0,0 +1,26 @@
+// const suger = parseInt('20');
+// console.log(suger);
+
+// const fatherAge = '50';
+// const fatherAgeNumber = parseInt(fatherAge);
+// console.log(fatherAgeNumber);
+
+// const input = 'x20';
+// const num = parseInt(input);
+// console.log(num);
+
+// const age = parseInt('twenty');
+// console.log(age);
+
+// const age = parseInt('50f');
+// const address = parseInt('tom30');
+// console.log(age, address);
+
+// const suger = parseFloat('1.222222222');
+// console.log(suger);
+
+
+const first = 12.13568;
+const second = 31.22345;
+const total = first + second;
+console.log(total.toFixed(2));
