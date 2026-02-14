4497b21cab072d5e73f02b5ad4556792edb74573
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-02-14 22:11:11 +0600

    comparison

--- comparison/comparison.js
+++ comparison/comparison.js
@@ -18,3 +18,19 @@
 
 // console.log( 75 == 75);
 
+const peyaraPrice = 40;
+const applePrice = 250;
+// console.log(peyaraPrice > applePrice);
+// console.log(peyaraPrice < applePrice);
+
+const myName = 'Rahim';
+const friendName = 'Rahim';
+// console.log(myName == friendName);
+
+const myPromise = true;
+const friendPromise = false;
+// console.log(myPromise == friendPromise);
+
+const didStudy = true;
+// console.log(didStudy != false);
+
