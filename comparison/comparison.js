73234b19d7a6f24faddeef8aecf8858e89afe92d
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-02-14 20:44:02 +0600

    comparison

---
+++ comparison/comparison.js
@@ -0,0 +1,20 @@
+// console.log(5 < 10);
+
+// console.log(5 == 10);
+
+// console.log(10 == 10);
+
+// console.log(10 >= 10);
+
+// console.log(15 >= 10);
+
+// console.log(5 <= 10);
+
+// console.log(7 != 10);
+
+// console.log(7 > 5);
+
+// console.log(45 < 50);
+
+// console.log( 75 == 75);
+
