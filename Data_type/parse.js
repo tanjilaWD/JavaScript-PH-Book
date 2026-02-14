97f1d4891b47ce5215a821496558e326c6439bdf
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-02-14 19:53:44 +0600

    parse

--- Data_type/parse.js
+++ Data_type/parse.js
@@ -20,7 +20,33 @@
 // console.log(suger);
 
 
-const first = 12.13568;
-const second = 31.22345;
-const total = first + second;
-console.log(total.toFixed(2));
+// const first = 12.13568;
+// const second = 31.22345;
+// const total = first + second;
+// console.log(total.toFixed(2));
+
+// const first = 12.13456;
+// const second = 31.22567;
+// const total = first + second;
+// const totalNumber = parseFloat(total.toFixed(2));
+// console.log(totalNumber);
+
+// let num = '20';
+// let result = Number(num) + 10;
+// console.log(result);
+
+// let value = '3.64159';
+// let integer = parseInt(value);
+// console.log(integer);
+
+// let x = Number('premikBoy');
+// console.log(x);
+
+// let sum = 3.456 + 2.789;
+// console.log(sum.toFixed(2));
+
+// let a = '56.78';
+// let b = '12.34';
+// let result = parseFloat(a) + parseFloat(b);
+// console.log(result);
+
