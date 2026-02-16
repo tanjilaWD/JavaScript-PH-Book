09cb7f74072e296309be89acd1b24ae6f5123914
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-02-16 11:43:50 +0600

    equal

---
+++ comparison/equal.js
@@ -0,0 +1,15 @@
+// console.log(10 == 10);
+
+// console.log(10 === '10');
+
+// console.log(120 === 'tomato');
+
+// console.log( 10 != 10);
+
+// console.log(10 != '10');
+
+// console.log(10 !== '10');
+
+// console.log(50 == '50')
+// console.log(50 === '50');
+
